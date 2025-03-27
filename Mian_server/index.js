551072f2a0e4b9fe8e5d60f99fb1b305662b6868
basha-masthan <bashamasthan31@gmail.com>
2025-03-28 00:09:39 +0530

    Test

--- Mian_server/index.js
+++ Mian_server/index.js
@@ -236,12 +236,12 @@ io.on('connection', (socket) => {
 
 
 // ======================================
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const dotenv = require('dotenv');
 
-dotenv.config();
+// dotenv.config();
 
 // const app = express();
 // app.use(express.json());
